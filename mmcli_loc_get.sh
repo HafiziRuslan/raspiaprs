4e6f9595cf621d999f54fa2c7e8c55ca9fe6a5c4
Hafizi Ruslan <pizi@hotmail.my>
2025-11-21 22:51:27 +0800

    🐛 fix(mmcli): correct modem index usage in location retrieval
    - use modem index from `mmcli -L` output instead of hardcoded `0`
    - ensures correct location retrieval when multiple modems are present

--- mmcli_loc_get.sh
+++ mmcli_loc_get.sh
@@ -5,19 +5,19 @@ IFS='/ ' read -r _ _ _ mm _ mn mt < <(mmcli -L)
 echo $mt
 
 echo -n "Latitude: "
-lat=$(mmcli -m 0 --location-get -J | jq .modem.location.gps.latitude | sed 's/\"//g')
+lat=$(mmcli -m $mn --location-get -J | jq .modem.location.gps.latitude | sed 's/\"//g')
 echo $lat
 
 echo -n "Longitude: "
-lon=$(mmcli -m 0 --location-get -J | jq .modem.location.gps.longitude | sed 's/\"//g')
+lon=$(mmcli -m $mn --location-get -J | jq .modem.location.gps.longitude | sed 's/\"//g')
 echo $lon
 
 echo -n "Altitude: "
-alt=$(mmcli -m 0 --location-get -J | jq .modem.location.gps.altitude | sed 's/\"//g')
+alt=$(mmcli -m $mn --location-get -J | jq .modem.location.gps.altitude | sed 's/\"//g')
 echo $alt
 
 echo -n "UTC: "
-utc=$(mmcli -m 0 --location-get -J | jq .modem.location.gps.utc | sed 's/\"//g')
+utc=$(mmcli -m $mn --location-get -J | jq .modem.location.gps.utc | sed 's/\"//g')
 echo $utc
 
 exit 1
