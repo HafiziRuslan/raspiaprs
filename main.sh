a92dea9d06b5a2c6dec336e053993af1ef7db6bd
Hafizi Ruslan <pizi@hotmail.my>
2025-12-10 20:28:57 +0800

    ✨ feat(deps): update project dependencies and tooling
    - [build] update release workflow to target v0.1-beta.2 for all assets
    - [build] update project version in pyproject.toml to v0.1-beta.2
    - [build] regenerate requirements.txt using uv, locking dependencies
    - [chore] update main.sh to use uv for environment management and dependency sync
    - [chore] replace venv with .venv directory for uv managed environment
    - [chore] install uv if not present in main.sh initialization script

--- main.sh
+++ main.sh
@@ -1,21 +1,41 @@
 #!/bin/bash
 set -e
 date=$(date +'%FT%T')
-if [ ! -d "venv" ]; then
-  echo $date "Virtual environment not found. Creating one."
-  python3 -m venv venv
-  echo $date  "Activating virtual environment"
-  source venv/bin/activate
-  echo $date  "Installing dependencies"
-  pip install -r requirements.txt
+
+command_exists() {
+  command -v "$1" >/dev/null 2>&1
+}
+
+if command_exists uv; then
+  echo $date "✅ uv is installed."
+  echo " -> Checking uv update"
+  uv self update
 else
-  echo $date "Virtual environment already exists."
-  echo $date "Activating virtual environment"
-  source venv/bin/activate
+  echo -n $date "❌ uv is NOT installed."
+  echo " -> Installing uv"
+  wget -qO- https://astral.sh/uv/install.sh | sh
+  echo 'eval "$(uv generate-shell-completion bash)"' >> ~/.bashrc
+  echo 'eval "$(uvx --generate-shell-completion bash)"' >> ~/.bashrc
 fi
+
+if [ ! -d ".venv" ]; then
+  echo $date "Virtual environment not found, creating one."
+  uv venv
+  echo $date "Activating virtual environment"
+  source .venv/Scripts/activate
+  echo $date "Installing dependencies"
+  uv sync
+else
+  echo -n $date "Virtual environment already exists."
+  echo " -> Activating virtual environment"
+  source venv/Scripts/activate
+  echo $date "Updating dependencies"
+  uv sync
+fi
+
 echo $date "Running main.py"
 while true; do
-  python3 ./main.py
+  uv run -s ./main.py
   echo $date "Script exited. Waiting for 15 seconds before the next run."
   sleep 15
 done
