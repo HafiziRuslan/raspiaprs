b4ec3cf0300a87986ff57106459f3705f84863c9
Hafizi Ruslan <pizi@hotmail.my>
2025-12-11 18:54:06 +0800

    ✅ test(gps): simplify gps data retrieval
    - remove redundant float casting for lat, lon, and alt as they are already floats or default to 0.
    - remove unnecessary conditional check for non-zero values before logging.
    - simplify satellite data retrieval by removing redundant int casting.

--- tests/gps.py
+++ tests/gps.py
@@ -25,13 +25,9 @@ def get_gpsd_position():
 				if result["class"] == "TPV":
 					logging.info("GPS fix acquired")
 					utc = result.get("time", dt.datetime.now(dt.timezone.utc))
-					lat = float(result.get("lat", 0))
-					lon = float(result.get("lon", 0))
-					alt = float(result.get("alt", 0))
-					if lat != 0 and lon != 0 and alt != 0:
-						logging.info(
-							"%s | GPS Position: %s, %s, %s", utc, lat, lon, alt
-						)
+					lat = result.get("lat", 0)
+					lon = result.get("lon", 0)
+					alt = result.get("alt", 0)
 					print(utc, lat, lon, alt)
 				else:
 					logging.info("GPS Position unavailable")
@@ -51,8 +47,8 @@ def get_gpsd_sat():
 			for result in client.json_stream(filter=["SKY"]):
 				if result["class"] == "SKY":
 					logging.info("GPS Satellite acquired")
-					uSat = int(result.get("uSat", 0))
-					nSat = int(result.get("nSat", 0))
+					uSat = result.get("uSat", 0)
+					nSat = result.get("nSat", 0)
 					print(uSat, nSat)
 				else:
 					logging.info("GPS Satellite unavailable")
